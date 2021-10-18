14e8748dd2e0fef227d2f0abb7a59a794ee0ff79
emtwnty <44481121+muizahsanu@users.noreply.github.com>
2021-10-18 23:11:21 +0700

    Create navbar

---
+++ app/js/script.js
@@ -0,0 +1,4 @@
+function ToggleSideBar(){
+    const getNavSide = document.querySelector('.nav .nav_side')
+    getNavSide.classList.toggle('show')
+}
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";function ToggleSideBar(){document.querySelector(".nav .nav_side").classList.toggle("show")}
 //# sourceMappingURL=script.js.map
