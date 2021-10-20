55a580391976082e985812f37e59f08aa03405d3
emtwnty <44481121+muizahsanu@users.noreply.github.com>
2021-10-20 22:54:08 +0700

    Delete log on code

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# e-commerce-product-page
- Challenge from Frontend Mentor
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Links
+
+- Solution URL: Coming-soon
+- Live Site URL: Coming-soon
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+- [SCSS](https://sass-lang.com/)
+
+### What I learned
+
+<!-- Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+To see how you can add code snippets, see below:
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+``` -->
+
+Delete object in array using 'filter' and 'includes'. I'm using this to delete item in Cart. Here the code:
+```js
+cart = cart.filter(item=>{
+    return !item.id.toString().includes(id.toString())
+})
+```
+
+### Continued development
+
+I want to learn more about creating slideshows. Like a slideshow that can display more than one item, or display many cards.
+
+### Useful resources
+
+- [CSS Scan - Shadow Examples](https://getcssscan.com/css-box-shadow-examples) - This helps me to choose the shadow. On this website there are 90+ examples of shadows.
+- [Lightbox](https://www.w3schools.com/howto/howto_js_lightbox.asp) - This site showing how to create modal image gallery (lightbox) with CSS and JavaScript.
+
+## Author
+
+<!-- - Website - [Add your name here](https://www.your-site.com) -->
+- Frontend Mentor - [@muizahsanu](https://www.frontendmentor.io/profile/muizahsanu)
+- Linkedin - [Muiz Ahsanu](https://www.linkedin.com/in/muiz-ahsanu-3943a7158/)
+- Instagram - [@muiz.ahsanu](https://www.instagram.com/muiz.ahsanu/)
+
+
+## Acknowledgments
+
+Thanks to [Coder Coder](https://www.youtube.com/channel/UCzNf0liwUzMN6_pixbQlMhQ) for making a video about Frontend Mentor. Because before I knew "Frontend Mentor" I was always confused about what project to make to practice my coding skills. Thanks also to Frontend Mentor for making the challenge.
--- app/js/script.js
+++ app/js/script.js
@@ -97,25 +97,22 @@ function addToCart(){
     }
 
     if(cart.length > 0){
-        console.log('masuk if');
         cart.forEach((item,index)=>{
             if(item.id == product.id) cart[index] = dataCart
             else cart.push(dataCart)
         })
     }
     else{
-        console.log('masuk else');
         cart.push(dataCart)
     }
-    console.log(cart);
     updateCartUI()
 }
 
 updateCartUI()
 
-function HapusCartItem(asu){
+function DeleteCartItem(id){
     cart = cart.filter(item=>{
-        return !item.id.toString().includes(asu.toString())
+        return !item.id.toString().includes(id.toString())
     })
     updateCartUI()
 }
@@ -123,15 +120,19 @@ function HapusCartItem(asu){
 function updateCartUI(){
     const basket = document.querySelector('.cart .basket')
     const btnCheckout = document.querySelector('.btn-checkout')
+    const notifCart = document.querySelector('.nav .nav_cart .cart-count')
 
     if(cart.length == 0){
         basket.innerHTML = `
             <p>Your cart is empty</p>
         `
         btnCheckout.style.display = 'none'
+        notifCart.style.display = 'none'
     }
     else{
+        var totalItem = 0
         cart.forEach(item => {
+            totalItem += parseInt(item.quantity);
             basket.innerHTML = `
             <div class="cart_item">
                 <div class="cart_item_image">
@@ -143,10 +144,12 @@ function updateCartUI(){
                     <span>x ${item.quantity}</span>
                     <span class="odd">$${item.totalPrice}</span>
                 </div>
-                <svg class="btn-delete" width="14" onclick="HapusCartItem(${item.id})" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
+                <svg class="btn-delete" width="14" onclick="DeleteCartItem(${item.id})" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
             </div>
             `
         }) 
         btnCheckout.style.display = 'block'
+        notifCart.innerHTML = totalItem
+        notifCart.style.display = 'block'
     }
 }
