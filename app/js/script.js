75294894620f9ecbc42c7f8b4e20108ba8db6ebf
emtwnty <44481121+muizahsanu@users.noreply.github.com>
2021-10-20 15:07:08 +0700

    Lightbox
    javascript functional

--- app/js/script.js
+++ app/js/script.js
@@ -9,3 +9,32 @@ function ShowCart(){
     getCart.classList.toggle('show')
     getOutsideCart.classList.toggle('show')
 }
+
+var lightboxIndex = 1
+showSlide(lightboxIndex)
+
+function currentSlide(n){
+    showSlide(lightboxIndex = n)
+}
+
+function showSlide(n){
+    const itemPreviews = document.getElementsByClassName('previews')
+    const items = document.getElementsByClassName('item')
+
+    // Validasi
+    if(n > itemPreviews.length) lightboxIndex = 1
+    if(n < 1) lightboxIndex = itemPreviews.length
+
+    // Set the previews display to none
+    for(let i = 0; i < itemPreviews.length; i++){
+        itemPreviews[i].style.display = 'none'
+    }
+
+    // Reset the active item
+    for(let i = 0; i < items.length; i++){
+        items[i].classList.remove('active')
+    }
+
+    itemPreviews[lightboxIndex-1].style.display = 'block'
+    items[lightboxIndex-1].classList.add('active')
+}
